66f5e4ec3f04f428f57fdce401babf88251ac8df
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2024-03-05 16:47:15 +0100

    fhudahf

--- database.sql
+++ database.sql
@@ -1,36 +1,37 @@
 -- Create table for 'Ansatt'
 CREATE TABLE Ansatt (
     AnsattID INT PRIMARY KEY,
-    Navn VARCHAR(255),
-    Epost VARCHAR(255),
+    Navn VARCHAR(50) NOT NULL,
+    Epost VARCHAR(50) NOT NULL,
+    AnsattStatus VARCHAR(50) NOT NULL
 );
 
 -- Create table for 'Direktør'
-CREATE TABLE Direktør (
-    DirektørID INT PRIMARY KEY,
+CREATE TABLE Direktor (
+    DirektorID INT PRIMARY KEY,
     AnsattID INT,
-    FOREIGN KEY (AnsattID) REFERENCES Ansatt(AnsattID)
+    FOREIGN KEY (AnsattID) REFERENCES Ansatt(AnsattID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create table for 'Skuespiller'
 CREATE TABLE Skuespiller (
     SkuespillerID INT PRIMARY KEY,
     AnsattID INT,
-    FOREIGN KEY (AnsattID) REFERENCES Ansatt(AnsattID)
+    FOREIGN KEY (AnsattID) REFERENCES Ansatt(AnsattID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create table for 'Rolle'
 CREATE TABLE Rolle (
     RolleID INT PRIMARY KEY,
-    RolleNavn VARCHAR(255)
+    RolleNavn VARCHAR(50) NOT NULL
 );
 
 -- Create table for 'HarRolle'
 CREATE TABLE HarRolle (
     SkuespillerID INT,
     RolleID INT,
-    FOREIGN KEY (SkuespillerID) REFERENCES Skuespiller(SkuespillerID),
-    FOREIGN KEY (RolleID) REFERENCES Rolle(RolleID),
+    FOREIGN KEY (SkuespillerID) REFERENCES Skuespiller(SkuespillerID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (RolleID) REFERENCES Rolle(RolleID) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (SkuespillerID, RolleID)
 );
 
@@ -39,8 +40,8 @@ CREATE TABLE RolleIAkt (
     AktNr INT,
     StykkeID INT,
     RolleID INT,
-    FOREIGN KEY (AktNr, StykkeID) REFERENCES Akt(AktNr, StykkeID),
-    FOREIGN KEY (RolleID) REFERENCES Rolle(RolleID),
+    FOREIGN KEY (AktNr, StykkeID) REFERENCES Akt(AktNr, StykkeID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (RolleID) REFERENCES Rolle(RolleID) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (AktNr, StykkeID, RolleID)
 );
 
@@ -48,26 +49,26 @@ CREATE TABLE RolleIAkt (
 CREATE TABLE Akt (
     AktNr INT,
     StykkeID INT,
-    Navn VARCHAR(255),
+    Navn VARCHAR(50) NOT NULL,
     PRIMARY KEY (AktNr, StykkeID),
-    FOREIGN KEY (StykkeID) REFERENCES TeaterStykke(StykkeID)
+    FOREIGN KEY (StykkeID) REFERENCES TeaterStykke(StykkeID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create table for 'TeaterStykke'
 CREATE TABLE TeaterStykke (
     StykkeID INT PRIMARY KEY,
-    Navn VARCHAR(255),
-    SalNr INT,
-    FOREIGN KEY (SalNr) REFERENCES TeaterSal(SalNr)
+    Navn VARCHAR(50) NOT NULL,
+    SalNr INT NULL, -- NULL if not performed in a theater
+    FOREIGN KEY (SalNr) REFERENCES TeaterSal(SalNr) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- Create table for 'HarOppgave'
 CREATE TABLE HarOppgave (
     StykkeID INT,
     AnsattID INT,
-    OppgaveNavn VARCHAR(255),
-    FOREIGN KEY (StykkeID) REFERENCES TeaterStykke(StykkeID),
-    FOREIGN KEY (AnsattID) REFERENCES Ansatt(AnsattID),
+    OppgaveNavn VARCHAR(50) NOT NULL,
+    FOREIGN KEY (StykkeID) REFERENCES TeaterStykke(StykkeID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (AnsattID) REFERENCES Ansatt(AnsattID) ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (StykkeID, AnsattID)
 );
 
@@ -76,24 +77,23 @@ CREATE TABLE TeaterOppsettning (
     Dato DATE,
     StykkeID INT,
     PRIMARY KEY (Dato, StykkeID),
-    FOREIGN KEY (StykkeID) REFERENCES TeaterStykke(StykkeID)
+    FOREIGN KEY (StykkeID) REFERENCES TeaterStykke(StykkeID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create table for 'TeaterSal'
 CREATE TABLE TeaterSal (
     SalNr INT PRIMARY KEY,
-    Navn VARCHAR(255),
-    Kapasitet INT
+    Navn VARCHAR(50) NOT NULL,
 );
 
 -- Create table for 'Sete'
 CREATE TABLE Sete (
     SalNr INT,
     SeteNr INT,
-    RadNr INT,
-    Område VARCHAR(255),
+    RadNr INT NULL, -- Can be null i Område GALLERI (ØVRE/NEDRE) in Sal HovedScene
+    Område VARCHAR(50) NOT NULL,
     PRIMARY KEY (SalNr, SeteNr),
-    FOREIGN KEY (SalNr) REFERENCES TeaterSal(SalNr)
+    FOREIGN KEY (SalNr) REFERENCES TeaterSal(SalNr) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create table for 'Billett'
@@ -103,12 +103,11 @@ CREATE TABLE Billett (
     Dato DATE,
     SalNr INT,
     SeteNr INT,
-    BillettType VARCHAR(255),
-    Pris DECIMAL(10, 2),
+    BillettType VARCHAR(50) NOT NULL,
     OrdreNr INT,
-    FOREIGN KEY (StykkeID, Dato) REFERENCES TeaterOppsettning(StykkeID, Dato),
-    FOREIGN KEY (SalNr, SeteNr) REFERENCES Sete(SalNr, SeteNr),
-    FOREIGN KEY (OrdreNr) REFERENCES Ordre(OrdreID)
+    FOREIGN KEY (StykkeID, Dato) REFERENCES TeaterOppsettning(StykkeID, Dato) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (SalNr, SeteNr) REFERENCES Sete(SalNr, SeteNr) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (OrdreNr) REFERENCES Ordre(OrdreID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Create table for 'PrisTabell'

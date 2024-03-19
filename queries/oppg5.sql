7efbe7a2436936b67bae414cf39bdc1bf89f1fb7
Åsmund Løvoll <aasmul@stud.ntnu.no>
2024-03-19 10:26:11 +0100

    Tirsdagsendringer

--- queries/oppg5.sql
+++ queries/oppg5.sql
@@ -1,7 +1,7 @@
-SELECT *
+SELECT DISTINCT StykkeNavn, Ansatt.Navn as SkuespillerNavn, RolleNavn
 FROM (
 	SELECT ts.navn as StykkeNavn, ts.StykkeID, a.AktNr, a.Navn as AktNavn, Rolle.RolleNavn, Rolle.RolleID 
-	FROM Akt as a
+	FROM Akt as a	
 	JOIN RolleIAkt as ria
 	ON a.AktNr = ria.AktNr AND a.StykkeID = ria.StykkeID
 	NATURAL JOIN Rolle
@@ -12,4 +12,4 @@ JOIN HarRolle as hr
 ON AktRoller.RolleID = hr.RolleID
 NATURAL JOIN Skuespiller
 NATURAL JOIN Ansatt
-ORDER BY Navn
+ORDER BY StykkeNavn

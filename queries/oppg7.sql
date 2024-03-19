52babcaa7f1af85e0a108e69a10bcbab552ed22e
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2024-03-19 15:50:54 +0100

    oppdaterte queries

--- queries/oppg7.sql
+++ queries/oppg7.sql
@@ -1,15 +1,36 @@
-SELECT *
-FROM (
-	SELECT ts.navn as StykkeNavn, ts.StykkeID, a.AktNr, a.Navn as AktNavn, Rolle.RolleNavn, Rolle.RolleID 
-	FROM Akt as a
-	JOIN RolleIAkt as ria
-	ON a.AktNr = ria.AktNr AND a.StykkeID = ria.StykkeID
-	NATURAL JOIN Rolle
-	JOIN TeaterStykke as ts
-	ON ts.StykkeID = a.StykkeID
-	) as AktRoller
-JOIN HarRolle as hr
-ON AktRoller.RolleID = hr.RolleID
-NATURAL JOIN Skuespiller
-NATURAL JOIN Ansatt
-ORDER BY Navn
+SELECT AktTilSkuespiller.Navn, SkuespillereInAkt.Navn, AktTilSkuespiller.AktNr, AktTilSkuespiller.StykkeNavn
+FROM
+(SELECT DISTINCT AktRoller.AktNr, AktRoller.StykkeID, AktRoller.StykkeNavn, Ansatt.Navn
+                    FROM (
+                        SELECT ts.navn as StykkeNavn, ts.StykkeID, a.AktNr, a.Navn as AktNavn, Rolle.RolleNavn, Rolle.RolleID 
+                        FROM Akt as a
+                        JOIN RolleIAkt as ria
+                        ON a.AktNr = ria.AktNr AND a.StykkeID = ria.StykkeID
+                        NATURAL JOIN Rolle
+                        JOIN TeaterStykke as ts
+                        ON ts.StykkeID = a.StykkeID
+                        ) as AktRoller
+                    JOIN HarRolle as hr
+                    ON AktRoller.RolleID = hr.RolleID
+                    NATURAL JOIN Skuespiller
+                    NATURAL JOIN Ansatt
+                    WHERE Ansatt.Navn = "Arturo Scotti"
+) as AktTilSkuespiller
+JOIN
+(SELECT DISTINCT Navn, Aktnr, StykkeID
+                    FROM (
+                        SELECT ts.navn as StykkeNavn, ts.StykkeID, a.AktNr, a.Navn as AktNavn, Rolle.RolleNavn, Rolle.RolleID 
+                        FROM Akt as a
+                        JOIN RolleIAkt as ria
+                        ON a.AktNr = ria.AktNr AND a.StykkeID = ria.StykkeID
+                        NATURAL JOIN Rolle
+                        JOIN TeaterStykke as ts
+                        ON ts.StykkeID = a.StykkeID
+                        ) as AktRoller
+                    JOIN HarRolle as hr
+                    ON AktRoller.RolleID = hr.RolleID
+                    NATURAL JOIN Skuespiller
+                    NATURAL JOIN Ansatt
+) as SkuespillereInAkt
+ON AktTilSkuespiller.AktNr = SkuespillereInAkt.AktNr AND AktTilSkuespiller.StykkeID = SkuespillereInAkt.StykkeID
+WHERE SkuespillereInAkt.Navn != AktTilSkuespiller.Navn -- Fjern spiller med seg selv.
--- queries/ordreOversikt.sql
+++ queries/ordreOversikt.sql
@@ -1,3 +1,11 @@
-SELECT *
+SELECT tstykke.Navn,  topps.Dato, tsal.Navn,  s.Omrade, s.RadNr, s.SeteNr, b.Pris
 FROM Ordre as o
-JOIN Billett
+JOIN Billett as b
+ON o.OrdreID = b.OrdreNr
+NATURAL JOIN Sete as s
+JOIN TeaterOppsettning as topps
+ON b.StykkeID = topps.StykkeID AND b.Dato = topps.Dato
+NATURAL JOIN TeaterStykke as tstykke
+JOIN TeaterSal as tsal
+ON s.SalNr = tsal.SalNr
+WHERE o.OrdreID = 2

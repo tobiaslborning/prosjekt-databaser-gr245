94613769a22e4f1cb8c75b729ff755cecfef1f6c
Tobias Liene Borning <tobiaslb@stud.ntnu.no>
2024-03-21 19:19:10 +0100

    validering brukerregistrering og endring fra OrdreID til OrdreNr i ordre tabell

--- queries/ordreOversikt.sql
+++ queries/ordreOversikt.sql
@@ -1,11 +1,11 @@
 SELECT tstykke.Navn,  topps.Dato, tsal.Navn,  s.Omrade, s.RadNr, s.SeteNr, b.Pris
 FROM Ordre as o
 JOIN Billett as b
-ON o.OrdreID = b.OrdreNr
+ON o.OrdreNr = b.OrdreNr
 NATURAL JOIN Sete as s
 JOIN TeaterOppsettning as topps
 ON b.StykkeID = topps.StykkeID AND b.Dato = topps.Dato
 NATURAL JOIN TeaterStykke as tstykke
 JOIN TeaterSal as tsal
 ON s.SalNr = tsal.SalNr
-WHERE o.OrdreID = 2
+WHERE o.OrdreNr = 2
